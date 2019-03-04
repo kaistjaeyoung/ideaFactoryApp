a3d3042eac86ada2efd9f01d7b018a88ae143233
ChangJaeYoung <wkdwodud07@kaist.ac.kr>
2019-03-04 23:23:51 +0900

    ADD : login,light button

---
+++ app/src/main/java/com/example/ideafactoryapp/LightControlActivity.kt
@@ -0,0 +1,12 @@
+package com.example.ideafactoryapp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class LightControlActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_light_control)
+    }
+}
--- app/src/main/java/com/example/ideafactoryapp/MainActivity.kt
+++ app/src/main/java/com/example/ideafactoryapp/MainActivity.kt
@@ -1,12 +1,24 @@
 package com.example.ideafactoryapp
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        sign_up_button.setOnClickListener {
+            val signUpIntent = Intent(this, LoginActivity::class.java)
+            startActivity(signUpIntent)
+        }
+
+        light_control_button.setOnClickListener {
+            val signUpIntent = Intent(this, LightControlActivity::class.java)
+            startActivity(signUpIntent)
+        }
     }
 }
